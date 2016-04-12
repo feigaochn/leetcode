731840384ae8d3b6ea70d2a8940bb5189e0fac20
Fei Gao <gaofei@iitserver.ihpc>
2016-04-12 15:32:22 +0800

    peeking iterator

--- peeking_iterator.py
+++ peeking_iterator.py
@@ -42,25 +42,25 @@
 
 
 # Below is the interface for Iterator, which is already defined for you.
-#
-# class Iterator(object):
-#     def __init__(self, nums):
-#         """
-#         Initializes an iterator object to the beginning of a list.
-#         :type nums: List[int]
-#         """
-#
-#     def hasNext(self):
-#         """
-#         Returns true if the iteration has more elements.
-#         :rtype: bool
-#         """
-#
-#     def next(self):
-#         """
-#         Returns the next element in the iteration.
-#         :rtype: int
-#         """
+
+class Iterator(object):
+    def __init__(self, nums):
+        """
+        Initializes an iterator object to the beginning of a list.
+        :type nums: List[int]
+        """
+
+    def hasNext(self):
+        """
+        Returns true if the iteration has more elements.
+        :rtype: bool
+        """
+
+    def next(self):
+        """
+        Returns the next element in the iteration.
+        :rtype: int
+        """
 
 class PeekingIterator(object):
     def __init__(self, iterator):
@@ -68,25 +68,39 @@ class PeekingIterator(object):
         Initialize your data structure here.
         :type iterator: Iterator
         """
-        
+        self.it = iterator
+        if self.it.hasNext():
+            self.top = self.it.next()
+        else:
+            self.top = None
 
     def peek(self):
         """
         Returns the next element in the iteration without advancing the iterator.
         :rtype: int
         """
+        return self.top
         
 
     def next(self):
         """
         :rtype: int
         """
+        if self.top is None:
+            raise StopIteration
+        ret = self.top
+        if self.it.hasNext():
+            self.top = self.it.next()
+        else:
+            self.top = None
+        return ret
         
 
     def hasNext(self):
         """
         :rtype: bool
         """
+        return self.top is not None
         
 
 # Your PeekingIterator object will be instantiated and called as such:
@@ -94,22 +108,16 @@ class PeekingIterator(object):
 # while iter.hasNext():
 #     val = iter.peek()   # Get the next element but not advance the iterator.
 #     iter.next()         # Should return the same value as [val].# TODO
-        return
 
 
 def main():
-    solver = Solution()
-    tests = [
-        (('param',), 'result'),
-    ]
-    for params, expect in tests:
-        print('-' * 5 + 'TEST' + '-' * 5)
-        print('Input:  ' + str(params))
-        print('Expect: ' + str(expect))
-
-        result = solver.__init__(*params)
-        print('Result: ' + str(result))
-    pass
+    it = PeekingIterator(iter([1,2,3]))
+    print(it.hasNext())
+    print(it.peek())
+    print(it.next())
+    print(it.next())
+    print(it.next())
+    print(it.hasNext())
 
 
 if __name__ == '__main__':
