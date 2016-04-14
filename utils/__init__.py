9f62c7693d006b1374c791bd5185fb9ae2f8c59c
Fei Gao <gaofei@iitserver.ihpc>
2016-04-14 15:25:58 +0800

    update utils for linked list

--- utils/__init__.py
+++ utils/__init__.py
@@ -37,3 +37,33 @@ def build_binary_tree(lst: list):
             nodes.append(top.right)
 
     return root
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def to_list(self):
+        vals = []
+        p = self
+        while p:
+            vals.append(p.val)
+            p = p.next
+        return vals
+
+    def __str__(self):
+        return str(self.to_list())
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def build_linked_list(lst: list):
+    if not lst:
+        return None
+    head = p = ListNode(lst[0])
+    for v in lst[1:]:
+        p.next = ListNode(v)
+        p = p.next
+    return head
