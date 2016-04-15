d8c4278a364c7ef294d3da45ef1a22257be5091d
Fei Gao <gaofei@iitserver.ihpc>
2016-04-15 16:56:02 +0800

    336. Palindrome Pairs

--- palindrome_pairs.py
+++ palindrome_pairs.py
@@ -7,30 +7,30 @@
 #     Given a list of unique words. Find all pairs of distinct indices (i, j)
 # in the given list, so that the concatenation of the two words, i.e. words[i]
 # + words[j] is a palindrome.
-# 
+#
 # Example 1:
 #     Given words = ["bat", "tab", "cat"]
 #     Return [[0, 1], [1, 0]]
 #     The palindromes are ["battab", "tabbat"]
-# 
+#
 # Example 2:
 #     Given words = ["abcd", "dcba", "lls", "s", "sssll"]
 #     Return [[0, 1], [1, 0], [3, 2], [2, 4]]
 #     The palindromes are ["dcbaabcd", "abcddcba", "slls", "llssssll"]
-# 
+#
 # Credits:Special thanks to @dietpepsi for adding this problem and creating
 # all test cases.
-# 
+#
 # Subscribe to see which companies asked this question
-# 
+#
 # Show Tags
-# 
+#
 # Hash Table
 # String
 # Trie
-# 
+#
 # Show Similar Problems
-# 
+#
 #  (M) Longest Palindromic Substring
 #  (H) Shortest Palindrome
 
@@ -41,37 +41,124 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[List[int]]
         """
-        # TODO
-        from collections import defaultdict
-        p_lr = defaultdict(set)
-        p_rl = defaultdict(set)
-        for iw, w in enumerate(words):
-            for i in range(1, len(w) + 1):
-                p_lr[w[:i]].add(iw)
-                p_rl[w[-i::-1]].add(iw)
-        print(p_lr, p_rl)
+        isit = lambda s: s == s[::-1]
         results = []
-        for iw, w in enumerate(words):
-            for j in p_rl[w]:
-                if j == iw:
+        wis = [(w, len(w), i) for i, w in enumerate(words)]
+        wis.sort(key=lambda p: p[1])
+        from collections import defaultdict
+        p_rl = dict()
+        p_l = set()
+        for w1, l1, i1 in wis:
+            # len(w1) >= len(w2)
+            for l2 in p_l:
+                if l2 == 0:
+                    if isit(w1):
+                        i2 = p_rl[""]
+                        results.append([i1, i2])
+                        results.append([i2, i1])
                     continue
-                nw = w + words[j][::-1]
-                if nw == nw[::-1]:
-                    results.append([iw, j])
-            for j in p_lr[w[::-1]]:
-                if j == iw:
-                    continue
-                nw = words[j] + w[::-1]
-                if nw == nw[::-1]:
-                    results.append([j, iw])
+                # w1 + w2:  w1 = w2.r + x
+                i2 = p_rl.get(w1[:l2], None)
+                if i2 is not None and isit(w1[l2:]):
+                    results.append([i1, i2])
+                # w2 + w1:  w1 = x + w2.r
+                i2 = p_rl.get(w1[l1-l2:], None)
+                if i2 is not None and isit(w1[:-l2]):
+                    results.append([i2, i1])
+            p_rl[w1[::-1]] = i1
+            p_l.add(l1)
+
         return results
 
 
 def main():
     solver = Solution()
     tests = [
+        ((["a", ""],), [[0, 1], [1, 0]]),
+        ((["a","abc","aba",""],), [[0,3],[3,0],[2,3],[3,2]]),
         ((["bat", "tab", "cat"],), [[0, 1], [1, 0]]),
         ((["abcd", "dcba", "lls", "s", "sssll"],), [[0, 1], [1, 0], [3, 2], [2, 4]]),
+        ((
+             ["bbaabaaaabbbbaaaabaaaabaabbabbbaaabbabbbabbaaababbaabbaaaabbbaabbaabaaaabaaabbaabab",
+              "babbbbbaabbbaaaaaabbbbbbbaabbabbabbbbbaababaabbabbbabbbbbbabba",
+              "bbbbaabbbababbabbbbabaababaabbaabbabaaaaabababbabbabbbbbabbbbaaaabbabbabbbabababbbaaa",
+              "ababbbbbbbaaaabbaaababbbabababbaabbaababbbabbbabaabbaabaababbbabababaaabbbbabbaa",
+              "abbbaababaaaaabababaabaaaaaabbbbbbbababaaaabaabbbaaabbaaaabbbabbbbabbaaaaaaaaaaabbaaaababbabbbabbabaaaaabbaababbabbabababaaaaabbabbababbaaaabbbababaaabbaaabbabbaaba",
+              "aabbaaaaaaabaaabbbababbbbabababaaabbbabaabbababbaabbbbbaabbbbaabaa",
+              "abbabaabbaaaaaababbbabbabababbababbbbaaaaabbbaaabbaababbababbbabbaaaabbabbbabbabaaaaaaabbabbbaababaaaabbaabaabababbaaaababbabbbabaabbabbbbaaaabbabbaabbaaaababbbbbbaaaaaababba",
+              "abbabaabbabbabbbabaabbbbbabaabbbbaabaaabaabaaabbaaaaaaababbababb",
+              "bbabbbbaaabbbbaaababbabaabbbbababaaaabbaaabbbaabbbaaabaababababaaaaabbbbaaaabbbababbabbbbaabbbbaaaaaaaaaabbabaaabaabaaa",
+              "aabaabbabbabbaaabbbbbaabbaababbabbaabbabaaaaaababaabaabbaaaabbabbbaababaaabaabbbbbaabbbbabbbbaaaaabbbbabbbbabaabaabbaabbbabaabababaabbaaaaabbabaaabbaabbbabaaaababbabbbababaaabbaa",
+              "bbbaaaaaaaabaababbabbabbbb",
+              "aabbaababbbbbbbabbbababaaaababbabaaabbaaaaabbbaaababbaaabbaaaaaaabbaabbaababbbbaabbaababbabbbbbbaaabaaaabbaabaababaabaab",
+              "aabbaabaabbabaabbbababbbaababbaaaababaaaaabaaaababbabbabababaaaabbabbbabbbbbbaababaaabbbababaabaaaabbbabaaa",
+              "aabbbbbbaabbabababbbbabbbbbaabbabaabbaaabbbb", "ababbabaaabaaa",
+              "babbaaaaabbaaabaaaabaabaabaaabaaaaaaabaaabba",
+              "bababaabbaaaaababbabaaabbbabababbbbbabbbbababb", "aaababbbbaabaabaaaabaabbabbababaaaaab",
+              "baabbbbbaabbaaaaababbabaabbabaaaabbabbbaabbabababababbaabaaabbbaabaabbbabbaaabbbbabababaabaabaabaabbbaaabaaabbbaaabaaaabbaaabbaaaababbbbabbaabbababbaabbbbbbababaaaabbbababa",
+              "baabbabaabaababbbaababbbbbabbbabbaaaaaaaabbbaabbaabaabbbabaabaabaabbaaabaaaabaaabaababaa",
+              "bbabbbabaababaabaaabbbbaaaaaabbbaaaaaaabababababbbbababbaaaaabbaabbabababbaaaabababbababbabbababbabbaa",
+              "aabbaabaabbaaabaaaaaabaaabbabababaaaabbabaabaaabbbabababbaabaaabbabbbbabbaabbababbbabbaaaabaaababbabbbbbbaaabbaababbabaabaaabaaabbbabaababaa",
+              "babaaababaabbabbabbbbbabaaabbaaabbabaabbabbabbbaabbbaaaaaabaabbaababbbbaaababbababbabbbabaababaabbabababbbabaabbbbaabaababbaa",
+              "bbabbababaaababababaabaaaaaaaabaababaababbabbabb",
+              "bbabbbbabbaaaabaabbbbbbabbbaaaaaaabbbbabbaaabbabaabbabaabbbbbbaabbbabaaabbabaaabbaba",
+              "baabbbbbbaaaaabbaabbbbabaaabbaaaabbbaaabbabaaababaababbbbbbabbaabaaabaaabbabbabaaaba",
+              "bbbaabaabbabababbaaabbaaaababaabbbbaabaaaaaaaaabbbbaabbbbabbbabababbabbbbababbaabbba",
+              "aabbbaabaababaaaabbbbabaabaabaaabababbaabbaabbaaabaababaaabbbabaaababaabbaababbbbaababaabaabbaababbababbbbbababababaabbabbaabaaaaaabaabbaabaabaaabaabba",
+              "abbabaababbbbbbaaaaaababbaabaabbaabbaaaababaababaabbbbbaaaabbbabbbbbaabbbbbbaaababbbaabaabababb",
+              "bbbaabaabaaaaabaabaabbbbaaaabaabbbbbaabbaaabbbbbababbbbabbaaaaabbaababababababbbbbbbaaabaabbbaaaaababbbaabbabaabababbbbaaabaabbababbbaabbaabbbbaaaabababbabaabbaaaaaababaaaaabaababaabbabaabbabababab",
+              "babaabaabaabbbabaaaabaabbbaaaaaaabaababbabbbaabbbabaababbabbaabbaaabaaaabaababbabbaababbabababbbabaabbbababbbbbbbaaababbaaaabaabaabbababbaaabbaabbbaaabaabaaaabaa",
+              "abbbababaaabbaabbbbabababbaabbaaaabaabaabbbbbbbbaabbbbaaaaabbbbabaaaaabbbaaaaabbbbaaaaaababbbbbaabbabaaaaabbaabaaabaababbabaabaaabbab",
+              "babaabaababababbbbabababbbaabbaaabbbaabbaaaabaaabbbbbaaaab", "aaaabbaabababbbaa",
+              "bbbbabbaabaabbbbaaaaaaaabbaababaaabaaabaaabbbbaababbbaabaabbbbaaabbabaabbbbbabbbbaaabababaabbaaababbabababbbabbabaabaaaabbab",
+              "aababaabaaaaaabbabababaaaaababbbbaabaa",
+              "bbababaaaaaaaaabaabbaaaabaaaaaaaaabbbabaabbbbbabaabaaabbbabbbabbbaaaaaabaaabbbbbbaa",
+              "abbaabaaaaabbaaaaaabbbbabaabbabbaabbbabbababaabaabaaabbababbaaabaababbbbbabbbabbabbaaaabbaababbaababbbabbbbbbaaabbbbbbb",
+              "baabababbbbbaabbbabaababaababaababbbabbbbbababbaababbaaaabaaabbbbaaaababbabbbabaabaabbababaaa",
+              "abbaabbbaabbabbabbbbbbbabbbabbaaabbaaababbaabbaababab",
+              "bbabbabbbbbbabababbbabaabbbaaaabababbaabbbabbbbaababbaabaaabbbbabaaaabbbabbaaabaabbaabbbaaababbaaaabaabbabbbabbbaaabaaabbbbabbaabbaabaa",
+              "aaaabbbabbaabbababbabbabaababbababaabbaaabaaaababbbabbaaaabbbbabaaabbbaaabababbbabbaabaaabbaabbbaabbbbaaabbbaabbbbababaaaaababbbabaabbbbbbabbabbbbbabaaabaaaaaabbaa",
+              "babaaabaabbaababaaabbaaababbbbbbbaabbbbbbbbabbaabbbbbbaaaaabbaabababaabbbabaaaaaaabbaabababaaabbabbabbaababbaaabbbaaaababbbbbabbbaabbbaaaabbabbaabbbaababbaaaabbaaaababbbbbb",
+              "abaababbaaabbbaaaaababaabbab",
+              "aabbaaaababbbabbbaaabaabababbabbbaabaaaabaabaaaabbabaabbaaabbbaaabbaabbabbbbaaabbbababbaabbaaaabbbabaabbbabbbbbbbaab",
+              "aabbbabbabaaaaabbabbababbbabbbbbbbbaabbbbabbbaabaabbaaababbbaaaaabbaaabbab",
+              "baaaabababaaababbaabbbabaaabbbbabbbbaaaabbabbbbbbbbababbaaaaabaabaaabbaabbbbbabababaabbbbaabbbbbabbaabaaabaababbbabbbabaababbaaaabbaaaaaabaababbbabaabbbaaabbbabbbaabbbbbbaabbabbbbabaabbbbaba",
+              "bbbaababaabaabbabbaabbabbaabbbbabaabbabbbbbbbabbaaaaababaaabbbaba",
+              "baaaaababaabaabaabbbbabbbbbaabbaabbaaababaabaababaabbaaabbabbbabbbaababababbaaaab",
+              "baabaaababbabbbaaaabbaabbaababbaababbbaabaabaaaaaabbbbababaaabaaabaababbaabbabbaababbbbbabbabbaabbaabbbabbbaabb",
+              "bbaaaaabbbaaabbbaaaaaaaaaabaaaaa",
+              "bbbbbbabaaaaabbbbaaabbabaababaabababbabbabaabababababbaabbabbbabbbababa",
+              "bbaabbbaabbabababaaaaaabbaabbababbbbabaabbbaabbabaabbbabbbabbaaabbabbababaaabbbababbabbbbbbbbaaaaabbaa",
+              "abaabbbaabbbbaabbabaabaaabaabaaaabbabbbabbaaabbbbababaabaaabaaaaaaaabaaaababbababaabbbbbaababaabbbbabbaaabbbabbabbbbabbbbaabaaa",
+              "bababbabbaaabbabaababbbbaabaabbbbbbababaabbbbaababababbaabbaabaaaababbabbabab",
+              "baaabbabbaaababbbbbbbaabaaabbabbaaabbabababbaaaabaabbbbaaaabbabbbbbbabaaaaabaabaaaaabababbbaabbaabbabbbbbabaababababbbbbbaabbbabbbaaabaabababaabaabbbbbbbbabaaaaabbaababab",
+              "ababababababbbabbabbbbbabbabbbbabbaaabbbbbbbbbbabbaabaabbbababbabbbaaaababbaabbbaab",
+              "baaaabbababbabaabbabbaabbbbabbbabbabbbbbabaaaabaabbbabbaababbabababaaaaabaaaababbbabbbaaaaabbbabbbaaaabbbbbaaaba",
+              "baababbbaaaabbbaaababbabaababbbbbaaabaabaaabababbbbaabbaaabaaabaaaaaabbbbbaabaaaababbbaabb",
+              "aaabaabbabaaaaaaaaaabbb", "abbbaabaabbbbbabbaababbaaaaababbaaaaabbabbbb",
+              "ababaabababbbabbbbaaabbabbbaaaaaaabbabbbaba",
+              "baababbaabbbaabbbbbbaaabbbbaaaabbbbabaabbaaabaaababbbaaabbaabaabaabaaabbbbbaabaaaaab",
+              "babbabababaabbbaabababaaaaaaaaaabbbbabbababbbaaaabbaabababbbbbbbababaaaababbaaaabaabaaabaabaaaaababbbbababbaabbaaabbabaaababbbbbabbbaaabaababbaabbabbbbaaaabababaaa",
+              "abbbbbaaaaabbaabbbabaabbabaabbbaaabbaaabaabbb", "abbababb",
+              "aababaaabbbabbbbaaaabaababbababbbaaaaabbababbbbaaaaaababababaabaaaabbbabbaababbbabbabaabbababbbbabbabbbabaaabababaababbbbbaaabaabaaaabbabbaaababbabaaaaa",
+              "abbbaaaaabaabbbabbbabbaaabbaababababbbaabbababaababaabbaaababbabbb",
+              "aaabaaabaabaababbabbbbaaaaaaaaabbbabbaabbababbaabababbbbbabbabababbabaaaabbbbabbbbbbbabbabaabababbaaabbaaaaaabbababbbbbbbaabbabbaaaababbabaaabbaabaaababaabaaaabaaba",
+              "baabbabbaabbbabbbbabbbaabbabaababaaaabaaaabbaaabaaababbabbbbbabaabbbaababaaababaababbaabaabaabababaaabbbbaabbbabbbbbba",
+              "abaababaaabaabbaabaaababbaabbbababbbaaaaaabbbabaaababbaabbbbaaabaabbaaabaaaababbabbbbbababbbaabbaaababaaaaaaababbbbbba",
+              "aaaabbaaaaaabbbaaaaabbaabbaabaabbababbbbbabababbababbabbaaaaabbaaabbabbaaabbbbbbaaabbbbbabbbbbabbabbaaaaaabbbbbaaaaaabbabbbbbbabbbaabbbaababbbbbabbaaabaa",
+              "ababbaaaababababbabaaabaaaaaabbbbababaababbababaabaabababaaaabbaaabaaaaaaabbbbababbabbbbabbbbbbabbbabaabbbabbbbbabbbbbababbbbaabbbaabbbababaaaabbbbaaabbab",
+              "bbabaababbabbbbbbaabbaaaabbaaabbabbaaabbabbaabaaabbbabbbababaabbbaaba",
+              "bbaaabbbaabaababbbababaaabbbbababbbbbaabaaabbbaabbbaaaaabbaabbaabaaabbabaababaaabbaababaabbbbbbaaabbabaabababaabbbaaaaabbbaaaabaabbababaaababaabbaabaaaaabaaaababbabaabababababaabaaaabaaabbaaabbbbababa",
+              "abbaaabbbaabbabb", "abbbabbaaaabaabaaababaaaaabababbbaabbbbaabbbaaa", "babaaabbbbbb",
+              "baaabaaabbbabaabbbaabaabbaaaabbbabbabbbbbaabbabbbabaabababaabbbbabaabaabbaaabbbabaaaaaaaaaabbbbaaabbaaaaaabaababbaabbabaabbbabaaabbbbaabbaababba",
+              "babababbbbabbaaaaaabbbbbaba", "babba", "abbbabaabbabbbbbbbbababaaaaaab",
+              "bbabbabbbabaaabbbaaababbbbaaaaaaabab",
+              "bbabbaabbbabaaaaaababaabbbabaaabaaababaabaaaaaaaaaaabbaababaababbababbabbababbbaaabbaaaabbaabbbbaaabbabbbabaabbbbbabababbaabaabbbabaabaaabaaababbbabbabbaababaabbbbaabaabbabbbbabbabbaababbbbba",
+              "bbaaabaaababaaaabbaabbabbbaabbaaabaababbaaababbbaaabaabaabbabababaaaabbbbbaaaabbbbbbababbbbabbbababbaababaababbbabaaabbabaaaabababaaabababaaabbbbbaaaaababaabbbbaabaaabba",
+              "ababaaaaaabbabbabba", "aaabababaabaaabaaaaabaaabaababbbbbababbbabbbbbbaabbaaaaaba", "baabbbab",
+              "abbaaaaababbbaaaabbbaaabbaaababbababababaabbabaabaaabaabaaabbaaabbababbaabaaaabaabababbbbbbabaabaaabaaaababbaababbaabbbbbaabbbbbbabaa",
+              "ababaaabbababbabbbaabbbaabaaaaaaaabbabaabaaaaaabaa"],
+         ), '?')
     ]
     for params, expect in tests:
         print('-' * 5 + 'TEST' + '-' * 5)
