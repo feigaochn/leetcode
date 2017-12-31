ecb978c277ff88803722c7d4efd8b5674dc6f747
Fei Gao <contact@feigao.org>
2017-12-31 22:53:08 +0800

    p748

---
+++ p748_largest_number_at_least_twice_of_others.py
@@ -0,0 +1,23 @@
+class Solution:
+    def dominantIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return -1
+        first, second = nums[:2]
+        large_idx = 0
+        if first < second:
+            first, second = second, first
+            large_idx = 1
+        for idx, other in enumerate(nums[2:], 2):
+            if other > first:
+                first, second = other, first
+                large_idx = idx
+            elif other > second:
+                second = other
+        if first >= 2 * second:
+            return large_idx
+        else:
+            return -1
